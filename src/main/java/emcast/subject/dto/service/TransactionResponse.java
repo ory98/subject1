a93f54daaf63dc414fa1fd2eadcfa8f25d35f673
ory98 <subin8360@naver.com>
2025-02-06 13:14:40 +0900

    응답값 추가

--- src/main/java/emcast/subject/dto/service/TransactionResponse.java
+++ src/main/java/emcast/subject/dto/service/TransactionResponse.java
@@ -1,9 +1,26 @@
 package emcast.subject.dto.service;
 
+import emcast.subject.domain.TransactionStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TransactionResponse {
+
+    private String userName;
+
+    private String bankName;
+
+    private String accountNumber;
+
+    private TransactionStatus status;
+
+    private BigDecimal balance; // 변경된 금액
+
+    private  BigDecimal currentTotalBalance; // 현재 잔여 잔액
 }
